542dbd5112f6e65bd531329b2b65df4999b13fb3
baboris <boris.benachour@gmail.com>
2024-04-19 09:19:21 +0200

    Ajout de la librairie AwesomeIcons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fontAwesomeIcons } from '../shared/font-awesome-icons';
+import { CommonModule } from '@angular/common';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'netflix-clone';
+
+  faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+      this.initFontAwesome();
+  }
+
+  private initFontAwesome(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
